b2d253fdb40ef6ba21f3355c7f0acc2bbe042f00
Hakase-Kong <95990747+Hakase-Kong@users.noreply.github.com>
2025-07-31 11:01:40 +0900

    Add files via upload

--- credit_issue_monitoring_news_sending.py
+++ credit_issue_monitoring_news_sending.py
@@ -1006,6 +1006,8 @@ def render_important_article_review_and_download():
                         important.append(new_article)
                         st.session_state["important_articles_preview"] = important
                         st.success("중요 기사 목록에 추가되었습니다: " + new_article["제목"])
+                        st.session_state.article_checked_left[from_key] = False   # <- 여기 추가
+                        st.session_state.article_checked[from_key] = False        # <- 여기 추가
                         st.rerun()
 
         with col_del:
